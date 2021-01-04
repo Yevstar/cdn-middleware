3f9733b003229a463aa533ee2f033ca0ae9f2e0d
Yevstar <fedchyshynyevhen@gmail.com>
2021-01-05 03:20:45 +0800

    acs-middleware updated - debug purpose

--- src/acs-middleware/index.js
+++ src/acs-middleware/index.js
@@ -220,7 +220,7 @@ const printMessage = async function (message) {
         let plctag
 
         for (let i = 0; i < numOfElements; i++) {
-          console.log(_machineId)
+          console.log(_machineId, commandNumber, deviceId)
           plctag = json_machines[_machineId - 1].full_json.plctags.find((tag) => {
             return tag.id === val.id
           })
@@ -376,8 +376,6 @@ module.exports = {
       })
 
       json_machines[0].full_json.plctags = db_batch_blender_plctags
-
-      console.log(json_machines[0].full_json.plctags)
     }
 
     senderClient = EventHubClient.createFromConnectionString(senderConnectionString, 'acsioteventhub1');
