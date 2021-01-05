f2e29bef94b4ec5ccaef0634a747d5a464a3abc6
Yevstar <fedchyshynyevhen@gmail.com>
2021-01-05 10:24:33 +0800

    GH Gravimetric Control Hopper simulator enabled

--- src/acs-middleware/index.js
+++ src/acs-middleware/index.js
@@ -85,8 +85,8 @@ const printMessage = async function (message) {
   // if (deviceId === 'TESTACS157') deviceId = 11234567157  // TrueTemp TCU
 
   // if (deviceId === 'TESTACS157') deviceId = 8880000001   // BD Batch Blender
-  // if (deviceId === 'TESTACS157') deviceId = 8880000001  // GH Gravimetric Extrusion Control Hopper
-  if (deviceId === 'TESTACS157') deviceId = 8880000002  // Accumeter Ovation Continuous Blender
+  if (deviceId === 'TESTACS157') deviceId = 8880000003  // GH Gravimetric Extrusion Control Hopper
+  // if (deviceId === 'TESTACS157') deviceId = 8880000002  // Accumeter Ovation Continuous Blender
   // if (deviceId === 'TESTACS157') deviceId = 9990000004  // GH-F Gravimetric Additive Feeder
   // if (deviceId === 'TESTACS157') deviceId = 5234567157  // VTC Plus Conveying System
   // if (deviceId === 'TESTACS157') deviceId = 6234567157  // NGX Dryer
