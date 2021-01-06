43525e3e1da1982cee6b1197a7d7a7a3020b1c90
Yevstar <fedchyshynyevhen@gmail.com>
2021-01-06 10:34:27 +0800

    T50 simulator enabled

--- src/acs-middleware/index.js
+++ src/acs-middleware/index.js
@@ -91,7 +91,7 @@ const printMessage = async function (message) {
   // if (deviceId === 'TESTACS157') deviceId = 8880000005  // VTC Plus Conveying System
   // if (deviceId === 'TESTACS157') deviceId = 8880000006  // NGX Dryer
   if (deviceId === 'TESTACS157') deviceId = 8880000007  // NGX Nomad Dryer
-  // if (deviceId === 'TESTACS157') deviceId = 8234567157  // T50 Central Granulator
+  if (deviceId === 'TESTACS157') deviceId = 8880000008  // T50 Central Granulator
   // if (deviceId === 'TESTACS157') deviceId = 9234567157  // GP Portable Chiller
   // if (deviceId === 'TESTACS157') deviceId = 10234567157  // HE Central Chiller
   // if (deviceId === 'TESTACS157') deviceId = 11234567157  // TrueTemp TCU
