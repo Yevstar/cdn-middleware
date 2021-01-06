a9a947c36442c421a43a62877e556df85e22341b
Yevstar <fedchyshynyevhen@gmail.com>
2021-01-06 12:47:16 +0800

    HE Chiller simulator enabled

--- src/acs-middleware/index.js
+++ src/acs-middleware/index.js
@@ -93,9 +93,9 @@ const printMessage = async function (message) {
   // if (deviceId === 'TESTACS157') deviceId = 8880000007  // NGX Nomad Dryer
   // if (deviceId === 'TESTACS157') deviceId = 8880000008  // T50 Central Granulator
   // if (deviceId === 'TESTACS157') deviceId = 8880000009  // GP Portable Chiller
-  if (deviceId === 'TESTACS157') deviceId = 88800000010  // HE Central Chiller
+  if (deviceId === 'TESTACS157') deviceId = 8880000010  // HE Central Chiller
   // if (deviceId === 'TESTACS157') deviceId = 11234567157  // TrueTemp TCU
-  
+
   let customerId = 0
   let machineId
   let _machineId
